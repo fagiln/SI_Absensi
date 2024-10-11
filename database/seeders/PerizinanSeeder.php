50247f743f7266fb69d33e202a0d68d591a0e27e
fagiln <fagilnuril18@gmail.com>
2024-10-11 09:32:01 +0700

    [seeder]

--- database/seeders/PerizinanSeeder.php
+++ database/seeders/PerizinanSeeder.php
@@ -21,13 +21,13 @@ class PerizinanSeeder extends Seeder
         // Loop melalui setiap user dan buat beberapa data perizinan untuk masing-masing
             Perizinan::create([
                 'user_id' => '2',
-                'start_date' =>'2024-10-17',
+                'start_date' =>'2024-10-11',
                 'end_date' => '2024-10-20',
                 'reason' => 'izin',
                 'keterangan' => 'Mengajukan izin karena sakit.',
                 'bukti_path' => 'path/to/bukti.jpg', // Sesuaikan path ini dengan lokasi bukti yang disimpan
                 // 'status' => 'pending',
-                'created_at' => '2024-10-16',
+                'created_at' => '2024-10-11',
 
             ]);
 
