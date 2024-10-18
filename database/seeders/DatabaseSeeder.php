fc511607f5158d79cdbf1cab308aa4ca02ca97e4
Diaz Bagus Maulana <94949041+Diaz49@users.noreply.github.com>
2024-10-18 14:58:33 +0700

    commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,46 +16,48 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    // public function run(): void
-    // {
-    //     // \App\Models\User::factory(10)->create();
+    public function run(): void
+    {
+         // \App\Models\User::factory(10)->create();
 
-    //     // \App\Models\User::factory()->create([
-    //     //     'name' => 'Test User',
-    //     //     'email' => 'test@example.com',
-    //     // ]);
+         // \App\Models\User::factory()->create([
+         //     'name' => 'Test User',
+         //     'email' => 'test@example.com',
+         // ]);
 
-    //     Department::create([
-    //         'kode_departemen' => 'MPA',
-    //         'nama_departemen'=> 'PT. Multi Power Abadi'
-    //     ]);Department::create([
-    //         'kode_departemen' => 'RJT',
-    //         'nama_departemen'=> 'Rajata Wedding Organizer Islami'
-    //     ]);
+        // Department::create([
+        //     'kode_departemen' => 'MPA',
+        //     'nama_departemen'=> 'PT. Multi Power Abadi'
+        // ]);
+        // Department::create([
+        //     'kode_departemen' => 'RJT',
+        //     'nama_departemen'=> 'Rajata Wedding Organizer Islami'
+        // ]);
         
-    //     User::create([
-    //         'nik' => '0000001',
-    //         'name' => 'Fagil Nuril Akbar',
-    //         'username' => 'fagil',
-    //         'jabatan' => 'Owner',
-    //         'email' => 'fagil@test.com',
-    //         'role' => 'admin',
-    //         'department_id' => '1',
-    //         'password' => Hash::make('123456')
+        // User::create([
+        //     'nik' => '0000001',
+        //     'name' => 'Fagil Nuril Akbar',
+        //     'username' => 'fagil',
+        //     'jabatan' => 'Owner',
+        //     'email' => 'fagil@test.com',
+        //     'role' => 'admin',
+        //     'department_id' => '1',
+        //     'password' => Hash::make('123456')
 
-    //     ]);  
-    //     // User::create([
-    //     //     'nik' => '0000002',
-    //     //     'name' => 'Shania Yan',
-    //     //     'username' => 'shania',
-    //     //     'jabatan' => 'CEO',
-    //     //     'email' => 'shanial@test.com',
-    //     //     'role' => 'user',
-    //     //     'department_id' => '2',
-    //     //     'password' => Hash::make('123456')
+        // ]);  
+        $faker = Faker::create();
+        User::create([
+            'nik' => $faker->unique()->numerify('######'),
+            'name' => $faker->name,
+            'username' => 'user1',
+            'jabatan' => $faker->randomElement(['CEO', 'Manager', 'Supervisor', 'Staff', 'Engineer']),
+            'email' => $faker->unique()->safeEmail,
+            'role' => 'user',
+            'department_id' => $faker->randomElement([1, 2]),
+            'password' => Hash::make('user123')
 
-    //     // ]);
-    // }
+        ]);
+    }
 
     // faker data 
     // public function run()
